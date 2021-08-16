c1aeb66d1b7608961d8ce5d21084090aea97e793
Valentinas Bartusevičius <valentinas.bartusevicius@eneba.com>
2021-08-16 15:22:07 +0300

    Remove PHP code

--- src/Denormalizer/Object/ArrayDenormalizer.php
+++ src/Denormalizer/Object/ArrayDenormalizer.php
@@ -14,7 +14,7 @@ class ArrayDenormalizer implements DenormalizerInterface, DenormalizerAwareInter
     public function denormalize($data, string $class): array
     {
         $out = [];
-        $baseClass = $this->getItemClass($class);
+        $baseClass = substr($class, 0, -2);
         foreach ($data as $item) {
             $out[] = $this->denormalizer->denormalize($item, $baseClass);
         }
@@ -24,20 +24,10 @@ class ArrayDenormalizer implements DenormalizerInterface, DenormalizerAwareInter
 
     public function supportsDenormalization(string $class): bool
     {
-        if ($this->isArrayClass($class)) {
-            return $this->denormalizer->supportsDenormalization($this->getItemClass($class));
+        if (substr($class, -2) === '[]') {
+            return $this->denormalizer->supportsDenormalization(substr($class, 0, -2));
         }
 
         return false;
-    }
-
-    private function isArrayClass(string $class): bool
-    {
-        return substr($class, -2) === '[]';
-    }
-
-    private function getItemClass(string $class): string
-    {
-        return substr($class, 0, -2);
     }
 }
